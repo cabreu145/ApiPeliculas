681d13f5dad7137c4f30ffa661ed54b3dd2d27ee
cabreu145 <cabreu145@gmail.com>
2019-11-19 19:47:22 -0600

    Inicio
    Hola

---
+++ api_pelis/api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Pelicula(models.Model):
+  titulo = models.CharField(max_length=150)
+  estreno = models.IntegerField(default=2000)
+  imagen = models.URLField(help_text="De imdb mismo")
+  resumen = models.TextField(help_text="Descripción corta")
+
+  class Meta:
+    ordering = ['titulo']
+
+class PeliculaFavorita(models.Model):
+  pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+  usuario = models.ForeignKey(User, on_delete=models.CASCADE)
---
+++ api_pelis/api/views.py
@@ -0,0 +1,59 @@
+from .models import Pelicula, PeliculaFavorita
+from .serializers import PeliculaSerializer, PeliculaFavoritaSerializer
+
+from django.shortcuts import get_object_or_404
+
+from rest_framework import viewsets, views
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+class PeliculaViewSet(viewsets.ModelViewSet):
+  queryset = Pelicula.objects.all()
+  serializer_class = PeliculaSerializer #convierte a formato json con  lo establecido en serializers.py
+
+
+class MarcarPeliculaFavorita(views.APIView):
+  authentication_classes = [TokenAuthentication] #tiene que estar autenticado
+  permission_classes = [IsAuthenticated] #autenticacion por token
+
+  # POST -> Se usa para crear un recurso sin un identificador
+  # PUT -> Se usa para crear/reemplazar un recurso con un identificador
+
+  def post(self, request):
+
+    pelicula = get_object_or_404(
+      Pelicula, id=self.request.data.get('id', 0)
+    )
+
+    favorita, created = PeliculaFavorita.objects.get_or_create(
+      pelicula=pelicula, usuario=request.user
+    )
+
+    # Por defecto suponemos que se crea bien
+    content = {
+      'id': pelicula.id,
+      'favorita': True
+    }
+
+    # Si no se ha creado es que ya existe, entonces borramos el favorito
+    if not created:
+      favorita.delete()
+      content['favorita'] = False
+
+    return Response(content)
+
+class ListarPeliculasFavoritas(views.APIView):
+  authentication_classes = [TokenAuthentication]
+  permission_classes = [IsAuthenticated]
+
+  # GET -> Se usa para hacer lecturas
+
+  def get(self, request):
+
+    peliculas_favoritas = PeliculaFavorita.objects.filter(
+      usuario=request.user)
+    serializer = PeliculaFavoritaSerializer(
+      peliculas_favoritas, many=True)
+
+    return Response(serializer.data)
